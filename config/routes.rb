f77cee04e5ada3ab96768f1a97843b16f3b7c593
alu <alu@xogrp.com>
2017-03-08 23:23:57 -0500

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'country/:country', to:'country#show'
+  get 'country/', to:'country#index'
 end
